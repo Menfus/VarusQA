6fa906d4e44f2981a351004d9640fe274960e64d
Menfus <Menfus369@mail.ru>
2025-04-08 17:02:43 +0300

    add method getMessage()

--- src/main/java/ru/VarusQA/Main.java
+++ src/main/java/ru/VarusQA/Main.java
@@ -2,6 +2,11 @@ package ru.VarusQA;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello MIR!");
-        }
+        System.out.println(getMessage());
     }
+
+
+    private static String getMessage() {
+        return "Hello MIR!";
+    }
+}
